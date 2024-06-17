fbe5d064470efd58abd419c0ea680956482a40d6
SoninDanil <sson1n312@gmail.com>
2024-06-17 14:29:03 +0500

    Add files via upload

---
+++ Hard/Choice.java
@@ -0,0 +1,8 @@
+
+public enum Choice {
+    PAPER,
+    ROCK,
+    SCISSORS,
+    LIZARD,
+    SPOCK
+}
---
+++ Hard/Spoke.java
@@ -0,0 +1,12 @@
+public class Spoke {
+
+    private Choice choice;
+
+    private Spoke() {
+        this.choice = Choice.SPOCK;
+    }
+
+    public Choice getChoice() {
+        return choice;
+    }
+}
